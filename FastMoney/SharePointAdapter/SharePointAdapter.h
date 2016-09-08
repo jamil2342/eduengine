365f09b75a6c5b06b242d9c9841ba35c19f97ae8
jamil_2342 <jamil_2342@6b4f22fb-dd74-41ec-b692-92a5a209e3ce>
2016-09-08 09:57:24 +0000

    added retrive list to parse xml

--- FastMoney/SharePointAdapter/SharePointAdapter.h
+++ FastMoney/SharePointAdapter/SharePointAdapter.h
@@ -24,6 +24,69 @@ public:
 	int Disconnect();
 
 public:
+
+
+	void RetrieveListItems1(wchar_t* url, VARIANT *vValues, int &ret)
+	{
+		CSharePointClient ^ _managedObject = gcnew CSharePointClient();
+		DataTable^ DT = _managedObject->GetFastMoneyDataTable(gcnew String(url));
+		//String^ error = _managedObject->LastError;
+		LONG lRow = 0;
+		SAFEARRAY *psa;
+		//LONG lStores = 0;
+		SAFEARRAYBOUND saBoundNew;
+		VARIANT vtTemp;
+		VariantInit(&vtTemp);
+		long ubound;
+
+		if (DT == nullptr)
+		{
+			return;
+		}
+		if (vValues->vt == (VT_ARRAY | VT_VARIANT))
+		{
+			psa = vValues->parray;
+			SafeArrayGetUBound(psa, 1, &ubound);
+			saBoundNew.lLbound = 0;
+			saBoundNew.cElements = ubound + 1 + (DT->Columns->Count * (DT->Rows->Count * 2) + 1);
+			SafeArrayRedim(psa, &saBoundNew);
+			lRow = ubound + 1;
+		}
+		else
+		{
+			VariantInit(vValues);
+			vValues->vt = VT_ARRAY | VT_VARIANT;
+			psa = SafeArrayCreateVector(VT_VARIANT, 0, DT->Columns->Count * (DT->Rows->Count * 2) + 1);
+		}
+
+		long lRow0 = 0;
+		int cnt = DT->Columns->Count;
+		vtTemp.vt = VT_I4;
+		vtTemp.lVal = cnt;
+		SafeArrayPutElement(psa, &lRow0, (void *)&vtTemp);
+		if (lRow == 0)
+			lRow++;
+
+		for each (DataRow^ row in DT->Rows)
+		{
+			for each (DataColumn^ col in DT->Columns)
+			{
+				Marshal::GetNativeVariantForObject(col->ColumnName, IntPtr(&vtTemp));
+				SafeArrayPutElement(psa, &lRow, (void *)&vtTemp);
+				lRow++;
+				VariantClear(&vtTemp);
+
+				Marshal::GetNativeVariantForObject(row[col], IntPtr(&vtTemp));
+				SafeArrayPutElement(psa, &lRow, (void *)&vtTemp);
+				VariantClear(&vtTemp);
+				lRow++;
+			}
+			//Console::WriteLine();
+		}
+		vValues->parray = psa;
+
+	}
+
 	void SetConfig(wchar_t* url,  wchar_t* proxy,  wchar_t* user,  wchar_t* password,  wchar_t* domain, wchar_t* authtype, enum ClientAuthenticationMode am, bool ntlm, int interval, int timeout);
 	void SetCertFile(wchar_t* certfile);
 	void SetFuagConfig(wchar_t* fuagAuth, wchar_t* userAgent);
